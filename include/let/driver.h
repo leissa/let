13bea17904ee97a4e752a4a27188124c232352f9
Roland Leißa <leissa@uni-mannheim.de>
2023-10-06 22:49:43 +0200

    missing file

---
+++ include/let/driver.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <fe/arena.h>
+#include <fe/driver.h>
+
+namespace let {
+
+class Driver : public fe::Driver {
+public:
+    template<class T, class... Args>
+    auto ast(Args&&... args) {
+        return arena_.mk<const T>(std::forward<Args&&>(args)...);
+    }
+
+private:
+    fe::Arena<> arena_;
+};
+
+} // namespace let
