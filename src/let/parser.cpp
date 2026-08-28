b032628593cc9ef523c1f21c8d0ec22e9885a7e2
Roland Leißa <roland.leissa@cs.uni-goettingen.de>
2026-08-29 00:22:00 +0200

    bump to new fe API

--- src/let/parser.cpp
+++ src/let/parser.cpp
@@ -69,10 +69,8 @@ AST<Expr> Parser::parse_primary_or_unary_expr(std::string_view ctxt) {
     }
 
     if (accept(Tag::D_paren_l)) {
-        auto _    = this->anchor(Tag::D_paren_r);
-        auto expr = parse_expr("parenthesized expression");
-        expect(Tag::D_paren_r, "parenthesized expression");
-        return expr;
+        auto _ = this->anchor(Tag::D_paren_r, "parenthesized expression");
+        return parse_expr("parenthesized expression");
     }
 
     if (!ctxt.empty()) {
